38e4f68f5a85bdd88df6b42d39bc0013eecc783b
Rudhi Mediastara <rudimediazzz@gmail.com>
2018-12-23 14:45:55 +0700

    adding  searchbar to navbar

--- components/Header/Main.js
+++ components/Header/Main.js
@@ -1,19 +1,26 @@
 import Link from "next/link";
 
-export default (props) => {
+export default props => {
   return (
-    <nav className="navbar-dark bg-dark">
-    <div className="d-flex row">
-    <button type="button" className="navbar btn btn-dark mr-3" data-toggle="collapse" onClick={props.clicked}>
-        <i className="navbar-toggler-icon" />
-      </button>
-      <Link href="/">
-        <a className="navbar-brand">Guds</a>
-      </Link>
-    
-    </div>
-
-     
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="d-flex row">
+        <button
+          type="button"
+          className="navbar btn btn-primary mr-3"
+          data-toggle="collapse"
+          onClick={props.clicked}
+        >
+          <i className="navbar-toggler-icon" />
+        </button>
+        <div className="mr-3">
+          <Link href="/">
+            <a className="navbar-brand">Guds</a>
+          </Link>
+        </div>
+        <div>
+          <input type="search" className="navbar mx-auto form-control-sm" />
+        </div>
+      </div>
     </nav>
   );
 };
